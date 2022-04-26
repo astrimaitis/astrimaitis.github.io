882e5d2003996c07118ec581b9d8b001b879be0c
Andrew Strimaitis <arstrimaitis@gmail.com>
2022-04-25 22:33:25 -0700

    hopefully this works

---
+++ website/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+export class Home extends Component {
+
+    render(){
+        return (
+            <>
+            <h1>This is Home.js</h1>
+            <h1>This is Home.js</h1>
+            </>
+        )
+    }
+
+}
+
